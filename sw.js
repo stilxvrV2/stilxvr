a8a2ec858d8a6e90304e40ded38a3b583a28f62b
stilxvrV2 <stilxleaked@gmail.com>
2025-10-24 01:20:28 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,33 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('stilx-cache').then(cache => {
-    return cache.addAll(['./', './index.html', './music.wav']);
-  }));
+const CACHE_NAME = 'stilxvr-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './music.wav',
+  './hoversound.wav',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
+// Activate event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
